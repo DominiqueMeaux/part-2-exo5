488c1aad702c389df7476246193eea477d3c4e71
dominiquem <dominique.meaux@gmail.com>
2018-06-25 14:20:27 +0200

    correction et indentation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
 //fonction qui permet de changer l'image au survol
 function imgChange(img){
-document.getElementById(img).src = 'assets/img/' + img + '_2.jpg' ;
+    document.getElementById(img).src = 'assets/img/' + img + '_2.jpg' ;
 }
