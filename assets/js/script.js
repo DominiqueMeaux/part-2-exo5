6151cbc8dd987f68d1dddb6261d0a311f66ab100
dominiquem <dominique.meaux@gmail.com>
2018-06-25 14:11:08 +0200

    correction part2.exo5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
-function ImgChange(id, img){
-  document.getElementById(id).src = img;
+//fonction qui permet de changer l'image au survol
+function imgChange(img){
+document.getElementById(img).src = 'assets/img/' + img + '_2.jpg' ;
 }
